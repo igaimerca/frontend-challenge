52d0943f330b973b6c9915d4ccc7d365040a5d29
igaimerca <igaimerca@gmail.com>
2022-03-26 16:10:44 +0200

    feat: added tabs

--- src/components/ui/MainContainer/MainContainer.jsx
+++ src/components/ui/MainContainer/MainContainer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar';
 import "./MainContainer.scss";
+import Tabs from './Tabs';
 
 function MainContainer() {
     return (
         <div className="main-container">
             <NavBar />
+            <div className="content">
+            <Tabs />
+            </div>
         </div>
     )
 }
