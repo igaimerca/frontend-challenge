e9768ad8d1777118e6a818a1fed86f71092a91bc
igaimerca <igaimerca@gmail.com>
2022-03-31 11:06:38 +0200

    feat: active tab

--- src/components/ui/MainContainer/Tabs.jsx
+++ src/components/ui/MainContainer/Tabs.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function Tabs() {
-  return (
-    <div className="tabs-container flex small">
-        <span className="active flex">Efficiency</span>
-        <span className="flex">Volume</span>
-        <span className="flex">Customer Satisfication</span>
-        <span className="flex">Backlog</span>
-    </div>
-  )
+function Tabs({ activeTab, changeTab }) {
+    return (
+        <div className="tabs-container flex small">
+            <span className={`${activeTab == "efficiency" && "active"} hover flex`}
+                onClick={() => changeTab('efficiency')}
+            >
+                Efficiency
+            </span>
+            <span
+                className={`${activeTab == "volume" && "active"} hover flex`}
+                onClick={() => changeTab('volume')}
+            >
+                Volume
+            </span>
+            <span className={`${activeTab == "customer" && "active"} hover flex`}
+                onClick={() => changeTab('customer')}
+            >
+                Customer Satisfication
+            </span>
+            <span className={`${activeTab == "backlog" && "active"} hover flex`}
+                onClick={() => changeTab('backlog')}
+            >
+                Backlog
+            </span>
+        </div>
+    );
 }
 
-export default Tabs
+export default Tabs;
