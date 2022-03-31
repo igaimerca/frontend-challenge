d48eca9b5e4c9f936df4557dc1cf020e169f9953
igaimerca <igaimerca@gmail.com>
2022-03-31 22:26:31 +0200

    feat: finished chart statistics cards

--- src/components/ui/ChartAverageCard/ChartAverageCard.jsx
+++ src/components/ui/ChartAverageCard/ChartAverageCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./ChartAverageCard.scss";
 
-function ChartAverageCard() {
+function ChartAverageCard({title, time}) {
     return (
-        <div className="chart-average-card">
-            ChartAverageCard
+        <div className="chart-average-card flex">
+            <div className="content flex column">
+                <p>{title}</p>
+                <b className="text-black x-large">{time}</b>
+            </div>
         </div>
     )
 }
