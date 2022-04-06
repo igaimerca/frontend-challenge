637a44795aa2155f07bbf3cb2bdd394be301c3a7
igaimerca <igaimerca@gmail.com>
2022-04-07 00:48:45 +0200

    feat: mobile version not supported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import Analytics from "./pages/Analytics/Analytics";
+import useWindowDimensions from "./utils/useWindowDimensions"
 
 function App() {
+    const { width, height } = useWindowDimensions();
+
     return (
         <>
-           <Analytics />
+        {width < 918 ? (
+            <div className="mobile-not-supported">
+                <h1>Use a desktop or laptop for better experience</h1>
+            </div>
+            ) : (
+            <Analytics />
+        )}
         </>
     );
 }
